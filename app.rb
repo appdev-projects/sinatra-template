81931658fc2a557c8518f68ed3d20aa376befa64
Ben Purinton <ben.purinton@gmail.com>
2023-06-09 13:28:14 -0800

    setup tasks routes and views

--- app.rb
+++ app.rb
@@ -5,9 +5,74 @@ require 'sinatra'
 # require active record to make use of it
 require 'sinatra/activerecord'
 
+# Define the Task model
+class Task < ActiveRecord::Base
+  # Attributes
+  # - id: integer (primary key)
+  # - name: string
+  # - description: text
+  # - created_at: datetime
+  # - updated_at: datetime
+end
+
+# add a homepage
 get('/') do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  redirect('/tasks')
+end
+
+# render a tasks index
+get('/tasks') do
+  @tasks = Task.all
+  erb(:tasks_index)
+end
+
+# create a new task
+get('/tasks/create') do
+  @name = params.fetch("query_task_name")
+  @description = params.fetch("query_task_description")
+
+  @task = Task.new
+  @task.name = @name
+  @task.description = @description
+  @task.save
+  
+  redirect('/tasks')
+end
+
+# show page for a task
+get('/tasks/:path_id') do
+  @task_id = params.fetch("path_id")
+
+  @list_of_tasks = Task.where({ :id => @task_id })
+  @task = @list_of_tasks.first
+
+  erb(:tasks_show)
+end
+
+# delete task
+get('/update_task/:path_id') do
+  @task_id = params.fetch("path_id")
+
+  @name = params.fetch("query_task_name")
+  @description = params.fetch("query_task_description")
+
+  @list_of_tasks = Task.where({ :id => @task_id })
+  @task = @list_of_tasks.first
+
+  @task.name = @name
+  @task.description = @description
+  @task.save
+  
+  redirect('/tasks/' + @task_id)
+end
+
+# edit task
+get('/delete_task/:path_id') do
+  @task_id = params.fetch("path_id")
+
+  @list_of_tasks = Task.where({ :id => @task_id })
+  @task = @list_of_tasks.first
+  @task.destroy
+
+  redirect('/tasks')
 end
