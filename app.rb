fe5c3b75816c293b6b38736488bc66b334853abd
Lamont Sampson <148111503+lsampson1974@users.noreply.github.com>
2024-01-22 03:10:28 +0000

    initial setup

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  
+  erb(:basic_calculator)
+
+
 end
