ccb2742a1ac3bcb1e09dc050dd8572fdc585b481
Hanna Slesarska <127249050+Hanna-Slesarska@users.noreply.github.com>
2024-06-05 19:22:14 +0000

    update

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require 'json'
 def getRecipe(search_term)
   API_ID = ENV.fetch("API_ID")
   API_KEY = ENV.fetch("API_KEY")
-  url = URI("https://api.edamam.com/api/recipes/v2?type=public&q=#{search_term}&app_id=0443a169&app_key=1ee0cbfaf95acc2db0be467e463a22ba")
+  url = URI("https://api.edamam.com/api/recipes/v2?type=public&q=#{search_term}&app_id=#{API_ID}&app_key=#{API_KEY}")
   response = Net::HTTP.get(url)
   response_obj = JSON.parse(response)
   results = response_obj.fetch("hits")
