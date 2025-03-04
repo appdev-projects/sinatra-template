253c46109edcbc0bf2fec76189ac93849f61f392
Torell T. Pernell <torellpernell@yahoo.com>
2025-03-03 22:12:03 -0600

    Update sinatra_banking_app.rb

--- sinatra_banking_app.rb
+++ sinatra_banking_app.rb
@@ -1,3 +1,26 @@
+require "sinatra"
+require "sinatra/reloader"
+
+get("/") do
+  erb(:home)
+end
+
+get("/process_roll?") do
+  @num_dice = params.fetch("dice")
+  @num_sides = params.fetch("sides")
+  
+ 
+  @rolls = []
+
+  @num_dice.to_i.times do 
+    die = rand(1..@num_sides.to_i)
+
+    @rolls.push(die)
+  end
+
+  
+  erb(:process_roll)
+end
 class BankAccount
   attr_reader :balance
 
